5957387cdeadeea9a270114b366a289f56f873a7
Pedro Dias <pedrocorchogdev@gmail.com>
2025-05-15 12:38:58 -0300

    feat: add initial signup page

--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -1,7 +1,40 @@
+import { Link } from 'react-router'
+
+import { Button } from '../components/ui/button'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '../components/ui/card'
+import { Input } from '../components/ui/input'
+
 const SignupPage = () => {
   return (
-    <div>
-      <h1>Signup Page</h1>
+    <div className="flex h-screen w-screen flex-col items-center justify-center gap-3">
+      <Card className="w-full max-w-md">
+        <CardHeader>
+          <CardTitle>Crie sua conta</CardTitle>
+          <CardDescription>Insira suas informações abaixo.</CardDescription>
+        </CardHeader>
+        <CardContent className="space-y-4">
+          <Input placeholder="Digite seu nome" />
+          <Input placeholder="Digite seu sobrenome" />
+          <Input placeholder="Digite seu email" />
+          <Input type="password" placeholder="Digite sua senha" />
+        </CardContent>
+        <CardFooter>
+          <Button className="w-full">Criar conta</Button>
+        </CardFooter>
+      </Card>
+      <div className="flex items-center justify-center">
+        <p className="text-center opacity-50">Ja possui uma conta?</p>
+        <Button variant="link" asChild>
+          <Link to="/login">Faça Login</Link>
+        </Button>
+      </div>
     </div>
   )
 }
