090d2848fe8cef8c80000aba317e85f471aa8838
Pedro Dias <pedrocorchogdev@gmail.com>
2025-05-16 16:02:38 -0300

    refactor: add helper functions and variables to auth context

--- src/contexts/auth.jsx
+++ src/contexts/auth.jsx
@@ -12,6 +12,19 @@ export const AuthContext = createContext({
 
 export const useAuthContext = () => useContext(AuthContext)
 
+const LOCAL_STORAGE_ACCESS_TOKEN_KEY = 'accessToken'
+const LOCAL_STORAGE_REFRESH_TOKEN_KEY = 'refreshToken'
+
+const setTokens = (tokens) => {
+  localStorage.setItem(LOCAL_STORAGE_ACCESS_TOKEN_KEY, tokens.accessToken)
+  localStorage.setItem(LOCAL_STORAGE_REFRESH_TOKEN_KEY, tokens.refreshToken)
+}
+
+const removeTokens = () => {
+  localStorage.removeItem(LOCAL_STORAGE_ACCESS_TOKEN_KEY)
+  localStorage.removeItem(LOCAL_STORAGE_REFRESH_TOKEN_KEY)
+}
+
 export const AuthContextProvider = ({ children }) => {
   const [user, setUser] = useState()
 
@@ -29,48 +42,6 @@ export const AuthContextProvider = ({ children }) => {
     },
   })
 
-  const signup = (data) => {
-    signupMutation.mutate(data, {
-      onSuccess: (createdUser) => {
-        const accessToken = createdUser.tokens.accessToken
-        const refreshToken = createdUser.tokens.refreshToken
-
-        setUser(createdUser)
-        localStorage.setItem('accessToken', accessToken)
-        localStorage.setItem('refreshToken', refreshToken)
-
-        toast.success('Conta criada com sucesso!')
-      },
-      onError: () => {
-        toast.error('Error ao criar conta, tente novamente mais tarde.')
-      },
-    })
-  }
-
-  useEffect(() => {
-    const init = async () => {
-      try {
-        const accessToken = localStorage.getItem('accessToken')
-        const refreshToken = localStorage.getItem('refreshToken')
-
-        if (!accessToken && !refreshToken) return
-
-        const response = await api.get('/users/me', {
-          headers: {
-            Authorization: `Bearer ${accessToken}`,
-          },
-        })
-
-        setUser(response.data)
-      } catch (error) {
-        localStorage.removeItem('accessToken')
-        localStorage.removeItem('refreshToken')
-        console.error(error)
-      }
-    }
-    init()
-  }, [])
-
   const loginMutation = useMutation({
     mutationKey: ['login'],
     mutationFn: async (data) => {
@@ -83,15 +54,25 @@ export const AuthContextProvider = ({ children }) => {
     },
   })
 
+  const signup = (data) => {
+    signupMutation.mutate(data, {
+      onSuccess: (createdUser) => {
+        setUser(createdUser)
+        setTokens(createdUser.tokens)
+
+        toast.success('Conta criada com sucesso!')
+      },
+      onError: () => {
+        toast.error('Error ao criar conta, tente novamente mais tarde.')
+      },
+    })
+  }
+
   const login = (data) => {
     loginMutation.mutate(data, {
       onSuccess: (loggedUser) => {
-        const accessToken = loggedUser.tokens.accessToken
-        const refreshToken = loggedUser.tokens.refreshToken
-
         setUser(loggedUser)
-        localStorage.setItem('accessToken', accessToken)
-        localStorage.setItem('refreshToken', refreshToken)
+        setTokens(loggedUser.tokens)
 
         toast.success('Login efetuado com sucesso!')
       },
@@ -100,6 +81,31 @@ export const AuthContextProvider = ({ children }) => {
       },
     })
   }
+
+  useEffect(() => {
+    const init = async () => {
+      try {
+        const accessToken = localStorage.getItem(LOCAL_STORAGE_ACCESS_TOKEN_KEY)
+        const refreshToken = localStorage.getItem(
+          LOCAL_STORAGE_REFRESH_TOKEN_KEY
+        )
+
+        if (!accessToken && !refreshToken) return
+
+        const response = await api.get('/users/me', {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        })
+
+        setUser(response.data)
+      } catch (error) {
+        removeTokens()
+        console.error(error)
+      }
+    }
+    init()
+  }, [])
 
   return (
     <AuthContext.Provider
