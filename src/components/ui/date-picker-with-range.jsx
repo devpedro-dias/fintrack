8a0227c80abb42393b81cb899715b5fd634d5ae7
Pedro Dias <pedrocorchogdev@gmail.com>
2025-05-18 18:24:52 -0300

    feat: add date picker with range component

---
+++ src/components/ui/date-picker-with-range.jsx
@@ -0,0 +1,44 @@
+'use client'
+
+import { format } from 'date-fns'
+import { CalendarIcon } from 'lucide-react'
+
+import { Button } from '@/components/ui/button'
+import { Calendar } from '@/components/ui/calendar'
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/components/ui/popover'
+import { cn } from '@/lib/utils'
+
+export function DatePickerWithRange({
+  value,
+  onChange,
+  placeholder = 'Selecione uma data',
+}) {
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        <Button
+          variant={'outline'}
+          className={cn(
+            'w-[240px] justify-start text-left font-normal',
+            !value && 'text-muted-foreground'
+          )}
+        >
+          <CalendarIcon />
+          {value ? format(value, 'PPP') : <span>{placeholder}</span>}
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-auto p-0" align="start">
+        <Calendar
+          mode="single"
+          selected={value}
+          onSelect={onChange}
+          initialFocus
+        />
+      </PopoverContent>
+    </Popover>
+  )
+}
