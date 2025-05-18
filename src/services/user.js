6a25418b6bb3839284ca3c707f434ecd9eac31bd
Pedro Dias <pedrocorchogdev@gmail.com>
2025-05-18 17:13:32 -0300

    refactor: add me method to user service

--- src/services/user.js
+++ src/services/user.js
@@ -2,22 +2,43 @@ import { protectedApi, publicAPi } from '@/lib/axios'
 
 export const UserService = {
   signup: async (input) => {
-    const repsonse = await protectedApi.post('/users', {
+    const response = await protectedApi.post('/users', {
       first_name: input.firstName,
       last_name: input.lastName,
       email: input.email,
       password: input.password,
     })
 
-    return repsonse.data
+    return {
+      id: response.data.id,
+      email: response.data.email,
+      firstName: response.data.first_name,
+      lastName: response.data.last_name,
+      tokens: response.data.tokens,
+    }
   },
 
   login: async (input) => {
-    const repsonse = await publicAPi.post('/auth/login', {
+    const response = await publicAPi.post('/auth/login', {
       email: input.email,
       password: input.password,
     })
 
-    return repsonse.data
+    return {
+      id: response.data.id,
+      email: response.data.email,
+      firstName: response.data.first_name,
+      lastName: response.data.last_name,
+      tokens: response.data.tokens,
+    }
+  },
+  me: async () => {
+    const response = await protectedApi.get('/users/me')
+    return {
+      id: response.data.id,
+      email: response.data.email,
+      firstName: response.data.first_name,
+      lastName: response.data.last_name,
+    }
   },
 }
